2631dc1fa803770736b7c62356950ecb36bdffe3
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2025-05-25 09:11:57 +0900

    feat: add release action

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {getVersion} from "@tauri-apps/api/app";
+
+getVersion().then(console.log);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
